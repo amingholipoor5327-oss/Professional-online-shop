04de2c3ab9e17b841d3809379bbc8b1e39b54020
amin <amingholipoor5327@gamil.com>
2026-09-06 17:31:36 +0330

    saved products in the database models and api saved

---
+++ apps/frontend/src/app/api/order/route.js
@@ -0,0 +1,26 @@
+import { connectDB } from "@/lib/mongodb";
+import Order from "@/models/Order";
+import { NextResponse } from "next/server";
+
+export async function POST(request ) {
+    try{
+    await connectDB() ;
+
+    const {user  , cart , totalprice } = await request.json()
+
+    const newOreder = new Order({
+        user  , 
+        cart , 
+        totalprice ,     
+    })
+
+     await newOreder.save()
+        return NextResponse.json(
+            {message: "سفارش شما با موفقیت ثبت شد "} , {status : 201}
+        )
+    }catch(error){
+
+        return NextResponse.json(
+            {message : "خطا در ثبت سفارش"}  , {status : 500})
+        }
+}
