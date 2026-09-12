4b18104ee312ec01716c1c191f1b57168c52ab0f
amin <amingholipoor5327@gamil.com>
2026-09-12 22:01:40 +0330

    basic files

---
+++ apps/admin/src/app/page.jsx
@@ -0,0 +1,19 @@
+ 
+export  async function generateMetadata({ }){
+    return{
+        title: "Home/your online cart",
+        description:  "this is a amagzing page for browzer online shop " , 
+
+        openGraph:{
+           title: "Home/your online cart",
+        description:  "this is a amagzing page for browzer online shop " ,   
+
+        }
+    }
+}
+export default function Home() {
+  return (
+    <div>
+    </div>
+  );
+}
